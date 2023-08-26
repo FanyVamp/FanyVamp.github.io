c6c1371c912d7d861e3a6cd46750b42b1dc6ec68
FanyVamp <129022321+FanyVamp@users.noreply.github.com>
2023-08-26 15:25:45 -0700

    Página Web Hecha Con Mucho Amor Y Cariño Por KC8
    V.2.1

--- Sources/Resources/ScrollSection.js
+++ Sources/Resources/ScrollSection.js
@@ -1,4 +1,4 @@
-const scrollToSection = sectionId =>
+var scrollToSection = sectionId =>
   document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
 
 window.onload = () =>
@@ -8,10 +8,3 @@ window.onload = () =>
       scrollToSection(link.hash.slice(1));
     })
   );
-  window.onload = () =>
-  document.querySelectorAll("nav a:not(.no-smooth-scroll)").forEach(link => {
-    link.addEventListener("click", event => {
-      event.preventDefault();
-      scrollToSection(link.hash.slice(1));
-    });
-  });
