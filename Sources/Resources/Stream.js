5dde2825cdc27f4516a9f9cff39855990e8ece78
FanyVamp <129022321+FanyVamp@users.noreply.github.com>
2024-02-16 00:10:54 -0700

    Add files via upload

--- Sources/Resources/Stream.js
+++ Sources/Resources/Stream.js
@@ -1,6 +1,4 @@
-
 $(document).ready(function() {
-            
     var twitchFany = new Twitch.Embed("twitch-embed-Fany", {
       width: 854,
       height: 480,
@@ -12,17 +10,22 @@ $(document).ready(function() {
       width: 854,
       height: 480,
       channel: "fanyvampart",
-      parent: ["fanyvamp.github.io"]
+      parent: ["fanyvamp.github.io"],
+      muted: true
     });
 
-    $('#carouselExampleControls').on('slid.bs.carousel', function (e) {
-      var index = $('.carousel-inner .carousel-item.active').index();
-      twitchFany.pause();
-      twitchFanyArt.pause();
-      if (index === 0) {
+    var carousel = document.getElementById('carouselExampleControls');
+    carousel.addEventListener('slide.bs.carousel', function (event) {
+      var activeItem = event.relatedTarget;
+      var activeIndex = $(activeItem).index() + 1;
+
+      if (activeIndex === 1) {
         twitchFany.play();
-      } else if (index === 1) {
+        twitchFanyArt.setMuted(true);
+      } else if (activeIndex === 2) {
+        twitchFany.setMuted(true);
         twitchFanyArt.play();
+        twitchFanyArt.setMuted(false);
       }
     });
   });
