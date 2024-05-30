6a0981adb202efe1492d0a57504f9be87980fbe3
FanyVamp <129022321+FanyVamp@users.noreply.github.com>
2024-05-30 14:00:43 -0700

    Add files via upload

--- Sources/Resources/Translate.js
+++ Sources/Resources/Translate.js
@@ -44,37 +44,70 @@ SeeDraws: "Aqui estão os desenhos feitos pela comunidade, para vê-los clique n
 dMAV:"Desenhos MAV"
 };
 
-function changeLanguage() {
-const language = navigator.language.split('-')[0];
-const translation = language === 'es' ? spanish : portuguese;
+var english = {
+First:"Home",
+Team:"Clan",
+Start:"Welcome to MAV!",
+Welcome: "Hello and welcome to Mav! We are a growing clan and we are excited to have both experienced players and newcomers to Death Ball. If you are just starting out in the game and looking to learn and improve, you are in the right place. We have with experienced members willing to share their knowledge and help you improve, without toxicity. To join MAV, we simply take a short test to get to know you better and make sure you find your place in our clan. So, if you are looking to join a community. fun and welcoming. Welcome to Mav!",
+News: "News",
+Community: "Community",
+Info:"At MAV we strive so that everyone can have fun and have a good time, so in our community we hold events, raffles and tournaments with great rewards. You can also exchange items, and live with other members to make friends. Do you like it? Would you like to join our community?",
+Clan:"Enter the clan",
+Join: "In order to enter the clan, you have to perform a skill test, there are two types of tests with different difficulties explained below:",
+Test: "Test type #1",
+TestC: "Character Skills Test",
+TestWOC: "Test without character skills",
+spamClick: "Spam click without diverting the ball",
+Test2: "Test type #2",
+Test2v2C: "2vs2 character skills test (You can choose your partner)",
+Test2vs2: "Test 2vs2 without character skills (You can choose your partner)",
+Focus: "Focus everyone against you",
+Draws: "Drawings",
+SeeDraws: "Here are the drawings made by the community, to see them, click on the ball:",
+dMAV:"MAV Drawings"
+};
 
-$('#navbarNav ul li:nth-child(1) a').text(translation.First);
-$('#navbarNav ul li:nth-child(2) a').text(translation.News);
-$('#navbarNav ul li:nth-child(3) a').text(translation.Community);
-$('#navbarNav ul li:nth-child(4) a').text(translation.Team);
-$('#navbarNav ul li:nth-child(5) a').text(translation.Draws);
-$('#Inicio h1').text(translation.Start);
-$('#Inicio p').text(translation.Welcome);
-$('#Novedades h1').text(translation.News);
-$('#Comunidad h1').text(translation.Community);
-$('#Comunidad p').text(translation.Info);
-$('#Clan h1').text(translation.Clan);
-$('#Clan p').text(translation.Join);
-$('#Dibujos h1').text(translation.Draws);
-$('#Clan .TypeOne .accordion-header button .fw-bold').text(translation.Test);
-$('#Clan .TypeOne .accordion-body ul li:nth-child(1)').text(translation.TestC);
-$('#Clan .TypeOne .accordion-body ul li:nth-child(2)').text(translation.TestWOC);
-$('#Clan .TypeOne .accordion-body ul li:nth-child(3)').text(translation.spamClick);
-$('#Clan .TypeTwo .accordion-header button .fw-bold').text(translation.Test2);
-$('#Clan .TypeTwo .accordion-body ul li:nth-child(1)').text(translation.TestC);
-$('#Clan .TypeTwo .accordion-body ul li:nth-child(2)').text(translation.TestWOC);
-$('#Clan .TypeTwo .accordion-body ul li:nth-child(3)').text(translation.Test2v2C);
-$('#Clan .TypeTwo .accordion-body ul li:nth-child(4)').text(translation.Test2vs2);
-$('#Clan .TypeTwo .accordion-body ul li:nth-child(5)').text(translation.spamClick);
-$('#Clan .TypeTwo .accordion-body ul li:nth-child(6)').text(translation.Focus);
-$('#Dibujos .container .col-md-8 p').text(translation.SeeDraws);
-$('#offcanvasRightLabel').text(translation.dMAV);
-$('#myModal .modal-content .modal-header .offcanvas-title').text(translation.Draws);
+function changeLanguage() {
+    const language = navigator.language.split('-')[0];
+    let translation;
+
+    if (language === 'es') {
+        translation = spanish;
+    } else if (language === 'pt') {
+        translation = portuguese;
+    } else if (language === 'en'){
+        translation = english;
+    } else {
+        translation = spanish;
+    }
+
+    $('#navbarNav ul li:nth-child(1) a').text(translation.First);
+    $('#navbarNav ul li:nth-child(2) a').text(translation.News);
+    $('#navbarNav ul li:nth-child(3) a').text(translation.Community);
+    $('#navbarNav ul li:nth-child(4) a').text(translation.Team);
+    $('#navbarNav ul li:nth-child(5) a').text(translation.Draws);
+    $('#Inicio h1').text(translation.Start);
+    $('#Inicio p').text(translation.Welcome);
+    $('#Novedades h1').text(translation.News);
+    $('#Comunidad h1').text(translation.Community);
+    $('#Comunidad p').text(translation.Info);
+    $('#Clan h1').text(translation.Clan);
+    $('#Clan p').text(translation.Join);
+    $('#Dibujos h1').text(translation.Draws);
+    $('#Clan .TypeOne .accordion-header button .fw-bold').text(translation.Test);
+    $('#Clan .TypeOne .accordion-body ul li:nth-child(1)').text(translation.TestC);
+    $('#Clan .TypeOne .accordion-body ul li:nth-child(2)').text(translation.TestWOC);
+    $('#Clan .TypeOne .accordion-body ul li:nth-child(3)').text(translation.spamClick);
+    $('#Clan .TypeTwo .accordion-header button .fw-bold').text(translation.Test2);
+    $('#Clan .TypeTwo .accordion-body ul li:nth-child(1)').text(translation.TestC);
+    $('#Clan .TypeTwo .accordion-body ul li:nth-child(2)').text(translation.TestWOC);
+    $('#Clan .TypeTwo .accordion-body ul li:nth-child(3)').text(translation.Test2v2C);
+    $('#Clan .TypeTwo .accordion-body ul li:nth-child(4)').text(translation.Test2vs2);
+    $('#Clan .TypeTwo .accordion-body ul li:nth-child(5)').text(translation.spamClick);
+    $('#Clan .TypeTwo .accordion-body ul li:nth-child(6)').text(translation.Focus);
+    $('#Dibujos .container .col-md-8 p').text(translation.SeeDraws);
+    $('#offcanvasRightLabel').text(translation.dMAV);
+    $('#myModal .modal-content .modal-header .offcanvas-title').text(translation.Draws);
 }
 
 $(document).ready(function() {
