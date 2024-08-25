f2f1e89fc1d974cce0401deb5cb7ec7e6fa7534e
FanyVamp <129022321+FanyVamp@users.noreply.github.com>
2024-08-24 20:29:32 -0700

    Add files via upload

--- Sources/Resources/YouTube.js
+++ Sources/Resources/YouTube.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
-
     var apiKey = 'AIzaSyDCZrsqNfF0nvOmHCMrufvaDZk92b0UbtY';
-    // Reemplaza con el ID del canal
     var channelId = 'UCQ3nubdTfWTLMKTTt9eja_A';
-    var videoTitle
     $.get(
         "https://www.googleapis.com/youtube/v3/search",{
             part: 'snippet',
@@ -16,13 +13,8 @@ $(document).ready(function(){
         function(data) {
             var videoId = data.items[0].id.videoId;
             var videoTitle = data.items[0].snippet.title;
-
-            // Accede al elemento lite-youtube y actualiza las propiedades directamente
-            var liteYoutubeElement = document.querySelector('lite-youtube');
-            liteYoutubeElement.videoId = videoId;
-            liteYoutubeElement.videoTitle = videoTitle;
+            $('lite-youtube').attr('videoid', videoId);
+            $('lite-youtube').attr('video-title', videoTitle);
         }
     );
-    $('lite-youtube').attr('videotitle', videoTitle);
-
 });
